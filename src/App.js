57b955885acb123f11f5ec819ae8a552185432e6
Ivan Cuella <ampersandwebs@gmail.com>
2022-05-31 22:49:04 -0300

    Routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import './App.css';
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Contact from './Pages/Contact';
 
 function App() {
   return (
     <>
-      <div className="App">
-          <header className="App-header">
-              <NavBar />
-          </header>
-          <ItemListContainer heading={'Bienvenidos a Musicommerce'} />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+        <header className="App-header">
+          <NavBar />
+          <Routes>
+            <Route exact path="/" />
+            <Route exact path="/contacto" element={ <Contact /> }/>
+          </Routes>
+        </header>
+        <ItemListContainer heading={'Bienvenidos a Musicommerce'} />
+        </div>
+      </BrowserRouter>
     </>
-  );
+  ); 
 }
 
 export default App;
