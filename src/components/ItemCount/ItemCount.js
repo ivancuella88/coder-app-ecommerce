b6475125dda69c32587ab4049891f2bfc40d6c83
Ivan cuella <ivan@folkamedia.com>
2022-06-02 22:12:56 -0300

    Desafío: Sincronizar counter

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -42,35 +42,44 @@ const ItemCount = ({id, defaulQty, stock, isSingleProduct}) => {
     }
 
     return (
-        <>
-            <div className="card-item__quantity">
-                <div className="card-item__quantity-button">
-                    <Button onClick={decrementQty} disabled={qty === defaulQty }>
-                        <svg xmlns="http://www.w3.org/2000/svg" width={20} height={20} fill="#2c2c2c" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M18 12H6" />
-                        </svg>
-                    </Button>
-                    <span className="card-item__quantity-button-value">{qty}</span>
-                    <Button onClick={IncrementQty} disabled={qty === stock || !qty }>
-                        <svg xmlns="http://www.w3.org/2000/svg" width={20} height={20} className="" fill="#2c2c2c" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
-                        </svg>
-                    </Button>
-                </div>
-            </div>
-            <div className="card-item__buttons-container">
-                <Button className="card-item__button card-item__add-to-cart-button" onClick={AddToCart} disabled={!stock}>Agregar al carrito</Button>
-                {
-                    !isSingleProduct && 
-                    <Link to={`/producto/${id}`}>
-                        <Button className="card-item__button card-item__view-product-button">Ver</Button>
-                    </Link>
-                }
-            </div>
-            {  
-                (qty > 0 && addedToCart) && (   
-                    <span className="card-item__added-to-cart-message">{AddedToCartMessage}</span>
+        <>  
+            {
+                !addedToCart
+                ? (
+                    <>
+                        <div className="card-item__quantity">
+                            <div className="card-item__quantity-button">
+                                <Button onClick={decrementQty} disabled={qty === defaulQty }>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width={20} height={20} fill="#2c2c2c" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M18 12H6" />
+                                    </svg>
+                                </Button>
+                                <span className="card-item__quantity-button-value">{qty}</span>
+                                <Button onClick={IncrementQty} disabled={qty === stock || !qty }>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width={20} height={20} className="" fill="#2c2c2c" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+                                    </svg>
+                                </Button>
+                            </div>
+                        </div>
+                        <div className="card-item__buttons-container">
+                            <Button className="card-item__button card-item__add-to-cart-button" onClick={AddToCart} disabled={!stock}>Agregar al carrito</Button>
+                            {
+                                !isSingleProduct && 
+                                <Link to={`/producto/${id}`}>
+                                    <Button className="default-button card-item__button card-item__view-product-button">Ver</Button>
+                                </Link>
+                            }
+                        </div>
+                    </>
                 )
+                : 
+                    <div className="flex">
+                        <Link to={'/carrito'}>
+                            <Button className="default-button card-item__button card-item__cart-link">Finalizar mi compra</Button>
+                        </Link>    
+                        <span className="card-item__added-to-cart-message">{AddedToCartMessage}</span>
+                    </div>
             }
         </>
     )
